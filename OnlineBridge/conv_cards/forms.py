7c1b07a28fa5e80f1fbcfed28474f894a943bb3a
Christian Bernscherer <cbernscherer@gmail.com>
2021-02-25 16:51:24 +0100

    convention card module new card prepared

---
+++ OnlineBridge/conv_cards/forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FileField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from OnlineBridge.users.models import Member
+
+
+class NewCardForm(FlaskForm):
+    partner = StringField('Partner', validators=[InputRequired(), Length(max=128)], render_kw={
+        'autofocus': True, 'list': 'memberlist'
+    })
+
+    conv_card = FileField('Konventionskarte (PDF)', validators=[InputRequired()])
+    submit = SubmitField('Hochladen')
+
+    def __init__(self, *args, **kwargs):
+        super(NewCardForm, self).__init__(*args, **kwargs)
+        self.partner_found = None
+
+    def validate_conv_card(self, field):
+        if not self.conv_card.data:
+            raise ValidationError('keine Datei ausgewählt')
+
+        if (len(self.conv_card.data.filename) < 4) or (self.conv_card.data.filename[-4:].lower() != '.pdf'):
+            raise ValidationError(f'Keine PDF-Datei')
+
+    def validate_partner(self, field):
+        if len(self.partner.data) == 0:
+            raise ValidationError('kein Partner angegeben')
+
+        partner_nr = str(self.partner.data).split()[-1]
+        self.partner_found = None
+
+        if partner_nr.isdigit():
+            self.partner_found = Member.query.filter_by(fed_nr=int(partner_nr)).one_or_none()
+        else:
+            self.partner_found = Member.query.filter_by(guest_nr=partner_nr).one_or_none()
+
+        if self.partner_found is None:
+            raise ValidationError('der Partner konnte nicht gefunden werden')
--- OnlineBridge/conv_cards/views.py
+++ OnlineBridge/conv_cards/views.py
@@ -1,3 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from OnlineBridge import db
+from flask_user import current_user, login_required, roles_required
+from OnlineBridge.users.models import Member
+from OnlineBridge.conv_cards.models import ConvCard
+from OnlineBridge.conv_cards.forms import NewCardForm
 
 conv_cards = Blueprint('conv_cards', __name__, template_folder='templates/conv_cards')
+
+@conv_cards.route('/new_card', methods=['GET', 'POST'])
+def new_card():
+    form = NewCardForm()
+
+    # get all the players with whom the current user has already cards stored
+
+    if request.method == "POST" and form.validate_on_submit():
+        pass
