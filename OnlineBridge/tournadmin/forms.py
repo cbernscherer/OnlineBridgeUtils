f7007a3eedd44ab73bbcd49784db1b20f688bbbc
Christian Bernscherer <cbernscherer@gmail.com>
2021-03-07 08:48:39 +0100

    parametersb form validation

--- OnlineBridge/tournadmin/forms.py
+++ OnlineBridge/tournadmin/forms.py
@@ -1,20 +1,42 @@
 # tournadmin
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, SelectField, SubmitField
+from wtforms import IntegerField, StringField, SelectField, SubmitField, RadioField
 from wtforms.validators import ValidationError, NumberRange, InputRequired, Length
 from wtforms.widgets import HiddenInput
 from OnlineBridge.tournadmin.models import ScoringMethod
 
 class ParameterForm(FlaskForm):
-    id = IntegerField('Schlüssel', validators=[InputRequired(), NumberRange(min=1, max=100)],
-                      render_kw={'autofocus': True})
-    name = StringField('Bezeichnung', validators=[InputRequired(), Length(max=64)])
-    tournament_type = SelectField('Torniertyp', choices=[('P', 'Paar'), ('T', 'Team')], default='T')
+    id = IntegerField('Schlüssel', validators=[
+        InputRequired(),
+        NumberRange(message='Nur Werte zwischen 0 und 100', min=0, max=100)])
+
+    name = StringField('Bezeichnung', validators=[InputRequired(), Length(message='Maximal 64 Zeichen', max=64)])
+    tournament_type = RadioField('Turniertyp', choices=[('P', 'Paar'), ('T', 'Team')], default='T')
     submit = SubmitField('Speichern')
 
-    def __init__(self, model, *args, **kwargs):
+    def __init__(self, new_param:bool, model, *args, **kwargs):
         super(ParameterForm, self).__init__(*args, **kwargs)
+        self.new_param = new_param
         self.model = model
 
         if self.model != ScoringMethod:
             self.tournament_type.widget = HiddenInput()
+
+        if self.new_param:
+            self.id.render_kw = {'autofocus': True}
+        else:
+            self.id.render_kw = {'disabled': True}
+            self.name.render_kw = {'autofocus': True}
+
+    def validate_id(self, field):
+        parameter = self.model.query.filter_by(id=self.id.data).one_or_none()
+
+        if self.new_param and parameter:
+            raise ValidationError('Diese ID existiert schon')
+
+    def validate_name(self, field):
+        parameter = self.model.query.filter_by(self.model.id.__ne__(self.id.data)).\
+            filter_by(name=self.name.data).one_or_none()
+
+        if parameter:
+            raise ValidationError('Diese Bezeichnung existiert schon')
--- OnlineBridge/tournadmin/views.py
+++ OnlineBridge/tournadmin/views.py
@@ -1,18 +1,20 @@
 # tournadmin
-
 from flask import Blueprint,request, render_template, redirect, abort, flash, url_for
 from flask_user import login_required, roles_required
 from OnlineBridge import db
 from OnlineBridge.tournadmin.models import VPScale, ScoringMethod, TimeDisplay
+from OnlineBridge.tournadmin.forms import ParameterForm
 
 tournadmin = Blueprint('tournadmin', __name__, template_folder='templates/tournadmin')
 
 
 @tournadmin.route('/vpscale/new', methods=['GET', 'POST'])
+@roles_required('Superuser')
 def new_vpscale():
     pass
 
 
 @tournadmin.route('/vpscale/<int:id>/update', methods=['GET', 'POST'])
-def new_vpscale(id):
+@roles_required('Superuser')
+def update_vpscale(id):
     pass
