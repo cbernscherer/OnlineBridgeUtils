a65aec5a14d9f9b435c750c7db2ef0dcbe280a50
Christian Bernscherer <cbernscherer@gmail.com>
2021-03-10 13:57:55 +0100

    views for general parameters

--- OnlineBridge/tournadmin/views.py
+++ OnlineBridge/tournadmin/views.py
@@ -8,64 +8,99 @@ from utilities import pagination_setup
 
 tournadmin = Blueprint('tournadmin', __name__, template_folder='templates/tournadmin')
 
+def get_param_model(param_type):
+    model = None
+    show_tourn_type = False
+    param_type_str = None
 
-@tournadmin.route('/vpscale/new', methods=['GET', 'POST'])
+    if param_type == 'vp_scale':
+        model = VPScale
+        param_type_str = ('VP Skala', 'VP Skalen', 'vp_scale')
+    elif param_type == 'time_display':
+        model = TimeDisplay
+        param_type_str = ('Zeitanzeige', 'Zeitanzeigen', 'time_display')
+    elif param_type =='scoring_method':
+        model = ScoringMethod
+        param_type_str = ('Scoremethode', 'Scoremethoden', 'scoring_method')
+        show_tourn_type = True
+
+    if model is None:
+        abort(404)
+
+    return model, param_type_str, show_tourn_type
+
+
+@tournadmin.route('/parameter/<string:param_type>/new', methods=['GET', 'POST'])
 @roles_required('Superuser')
-def new_vpscale():
+def new_param(param_type):
     per_page = 10
-    page, last_page = pagination_setup(per_page, VPScale)
 
-    form = ParameterForm(True, VPScale, -1)
+    model, param_type_str, show_tourn_type = get_param_model(param_type)
+
+    page, last_page = pagination_setup(per_page, model)
+
+    form = ParameterForm(True, model, -1)
 
     if request.method == "POST" and form.validate_on_submit():
-        vp_scale = VPScale(id=form.id.data, name=form.name.data)
-        db.session.add(vp_scale)
+        parameter = model(id=form.id.data, name=form.name.data)
+        if show_tourn_type:
+            parameter.tournament_type = form.tournament_type.data
+
+        db.session.add(parameter)
         db.session.commit()
 
-        return redirect(url_for('tournadmin.new_vpscale', page=page))
+        return redirect(url_for('tournadmin.new_param', page=page))
 
-    vp_scales = VPScale.query.order_by(VPScale.id.asc()).paginate(page=page, per_page=per_page)
+    parameters = model.query.order_by(model.id.asc()).paginate(page=page, per_page=per_page)
 
     context = {
         'form': form,
-        'parameters': vp_scales,
-        'show_tourn_type': False,
-        'new_param': True,
-        'param_type': ('VP Skala', 'VP Skalen', 'vp_scale')
+        'parameters': parameters,
+        'show_tourn_type': show_tourn_type,
+        'new_param': False,
+        'param_type': param_type_str
     }
     return render_template('parameter.html', **context)
 
 
-@tournadmin.route('/vpscale/<int:id>/update', methods=['GET', 'POST'])
+@tournadmin.route('/parameter/<string:param_type>/<int:id>/update', methods=['GET', 'POST'])
 @roles_required('Superuser')
-def update_vpscale(id):
+def update_paramm(param_type ,id):
     per_page = 10
-    page, last_page = pagination_setup(per_page, VPScale)
 
-    vp_scale = VPScale.query.filter_by(id=id).first_or_404()
+    model, param_type_str, show_tourn_type = get_param_model(param_type)
 
-    form = ParameterForm(False, VPScale, vp_scale.id)
+    page, last_page = pagination_setup(per_page, model)
+
+    parameter = model.query.filter_by(id=id).first_or_404()
+
+    form = ParameterForm(False, model, parameter.id)
 
     if request.method == "POST" and form.validate_on_submit():
-        vp_scale.name = form.name.data
-        db.session.add(vp_scale)
+        parameter.name = form.name.data
+        if show_tourn_type:
+            parameter.tournament_type = form.tournament_type.data
+
+        db.session.add(parameter)
         db.session.commit()
 
-        return redirect(url_for('tournadmin.new_vpscale', page=page))
+        return redirect(url_for('tournadmin.new_param', page=page, param_type=param_type))
 
     elif request.method == 'GET':
-        form.name.data = vp_scale.name
+        form.name.data = parameter.name
+        if show_tourn_type:
+            form.tournament_type.data = parameter.tournament_type
 
-    form.id.data = vp_scale.id
-    vp_scales = VPScale.query.order_by(VPScale.id.asc()).paginate(page=page, per_page=per_page)
+    form.id.data = parameter.id
+    parameters = model.query.order_by(model.id.asc()).paginate(page=page, per_page=per_page)
 
     context = {
         'form': form,
-        'parameters': vp_scales,
-        'this_param': vp_scale,
-        'show_tourn_type': False,
+        'parameters': parameters,
+        'this_param': parameter,
+        'show_tourn_type': show_tourn_type,
         'new_param': False,
-        'param_type': ('VP Skala', 'VP Skalen', 'vp_scale')
+        'param_type': param_type_str
     }
     return render_template('parameter.html', **context)
 
@@ -74,20 +109,10 @@ def update_vpscale(id):
 @roles_required('Superuser')
 def param_delete(param_type, id):
     if request.method == 'GET':
-        model = None
-
-        if param_type == 'vp_scale':
-            model = VPScale
-        elif param_type == 'time_display':
-            model = TimeDisplay
-        elif param_type =='scoring_method':
-            model = ScoringMethod
-
-        if not model:
-            abort(404)
+        model, param_type_str, show_tourn_type = get_param_model(param_type)
 
         parameter = model.query.filter_by(id=id).first_or_404()
         db.session.delete(parameter)
         db.session.commit()
 
-    return redirect(url_for('tournadmin.new_vpscale'))
+    return redirect(url_for('tournadmin.new_param', param_type=param_type))
